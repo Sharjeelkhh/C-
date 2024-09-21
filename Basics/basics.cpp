cfbeb80f53edd808e49b1cbe1cf6f756d10ef77f
Dobby San <khanwaheed1976skt@gmail.com>
2024-09-21 20:48:40 +0500

    updates

--- Basics/basics.cpp
+++ Basics/basics.cpp
@@ -1,6 +1,42 @@
+// #include <iostream>
+// using namespace std;
+                                        // Basic Output
+// int main() {
+//     cout << " Hi im Ali";
+// }
+
+/*  int a = 4;
+    double b = 3.3;
+    char c = 'b';    */
+
+
+
+
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    cout << " Hi im Ali";
+    
+    int a, b;
+    cout << "Enter the value of a: ";
+    cin >> a;                                                   //control structures
+    cout << "Enter the value of b: ";
+    cin >> b;
+
+    if (a>b)
+    {
+        cout << "The value of a is greater than b";
+    }
+    else
+        cout << "The value of b is greater than a";
+
+    int c, d;
+        cout << "\nEnter two Value u want the sum of: ";
+        cin >> c >> d;
+        cout << "The sum of two values is: "<< c + d;
+
+
 }
+
+
