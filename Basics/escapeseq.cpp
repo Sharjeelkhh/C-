96c23154cc7f398c94cc81f4caf6bc3f6a5795ad
Dobby San <khanwaheed1976skt@gmail.com>
2024-10-11 15:58:37 +0500

    add

---
+++ Basics/escapeseq.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << "\tMenu\n----------------------\nItem\t\tPrice\n----------------------\nBurger\t\t$1\nTea\t\t$1.5\nSandwich\t$2";
+}
---
+++ Basics/escapeseq.py
@@ -0,0 +1,1 @@
+print("Subject\t\tMarks\nMathematics\t90\nComputer\t77\nChemistry\t69")
